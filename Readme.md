bfed6e03249238e627d31ea90ca278ca5a2f0f4f
Sahil Harriram <sahil.harriram@gmail.com>
2023-06-30 01:41:36 +0700

    Revert "Revert "user account creation completed""
    This reverts commit b7f2358359af220eab45ffbc80f3f1ceea30ccf6.

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+
+# Usage
+
+1. Install:
+`pnpm install --node-linker=hoisted`
+2. Run:
+`pnpm run dev` 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,18 +9,12 @@ import {
   darkTheme,
   DisclaimerComponent,
 } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, useAccount, WagmiConfig } from "wagmi";
+import { createConfig, configureChains, WagmiConfig } from "wagmi";
 import { avalanche, avalancheFuji } from "wagmi/chains";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
 
-// RainbowKit Authentication
-import {
-  RainbowKitSiweNextAuthProvider,
-  GetSiweMessageOptions,
-} from "@rainbow-me/rainbowkit-siwe-next-auth";
 import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
 
 //import MainLayout from "../layout/mainLayout";
 import { useRouter } from "next/router";
@@ -32,29 +26,9 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "../styles/globals.css";
-import { RainbowKitChain } from "@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/RainbowKitChainContext";
 
-const Disclaimer: DisclaimerComponent = ({ Text, Link }) => (
-  <Text>
-    By connecting your wallet, you agree to the{" "}
-    <Link href="http://localhost:3000/terms_and_conditions">
-      Terms of Service
-    </Link>{" "}
-    and acknowledge you have read and understand the protocol{" "}
-    <Link href="https://disclaimer.xyz">Disclaimer</Link>
-  </Text>
-);
-
-const getSiweMessageOptions: GetSiweMessageOptions = () => ({
-  statement: "Sign in to BlocTrace",
-});
-
-const infuraApiKey: string | undefined = process.env.NEXT_PUBLIC_INFURA_API_KEY;
-if (!infuraApiKey || infuraApiKey.length <= 0) {
-  console.log("infura if", infuraApiKey);
-} else {
-  console.log("infura else", infuraApiKey);
-}
+import { mainnet } from "wagmi/chains";
+import { Session } from "next-auth";
 
 const walletConnectProjectId: string | undefined =
   process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
@@ -64,9 +38,15 @@ if (walletConnectProjectId) {
   console.log("walletConnectId else", walletConnectProjectId);
 }
 
-//
-const { chains, publicClient } = configureChains(
-  [avalanche, avalancheFuji],
+const infuraApiKey: string | undefined = process.env.NEXT_PUBLIC_INFURA_API_KEY;
+if (!infuraApiKey || infuraApiKey.length <= 0) {
+  console.log("infura if", infuraApiKey);
+} else {
+  console.log("infura else", infuraApiKey);
+}
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [avalancheFuji, avalanche],
   [infuraProvider({ apiKey: infuraApiKey! }), publicProvider()]
 );
 
@@ -78,10 +58,10 @@ const { connectors } = getDefaultWallets({
 
 const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors,
   publicClient,
+  webSocketPublicClient,
+  connectors,
 });
-// export { WagmiConfig, RainbowKitProvider };
 
 function MyApp({
   Component,
@@ -91,48 +71,23 @@ function MyApp({
 }>) {
   const router = useRouter();
 
-  let defaultChain: string;
-  if (process.env.NEXT_PUBLIC_DEFAULT_CHAIN) {
-    defaultChain = process.env.NEXT_PUBLIC_DEFAULT_CHAIN;
-  } else {
-    throw new Error(
-      "NEXT_PUBLIC_DEFAULT_CHAIN environment variable is not set"
-    );
-  }
-
-  const initialChain: number | RainbowKitChain = parseInt(
-    process.env.NEXT_PUBLIC_DEFAULT_CHAIN
-  );
-  const account = useAccount({
-    onConnect({ address, connector, isReconnected }) {
-      if (!isReconnected) router.reload();
-    },
-  });
   return (
     <ChakraProvider theme={myTheme}>
       <WagmiConfig config={wagmiConfig}>
         <SessionProvider refetchInterval={0} session={pageProps.session}>
-          <RainbowKitSiweNextAuthProvider
-            getSiweMessageOptions={getSiweMessageOptions}
+          <RainbowKitProvider
+            theme={darkTheme({
+              accentColor: "#D66E36",
+              accentColorForeground: "white",
+              borderRadius: "medium",
+              fontStack: "system",
+            })}
+            modalSize="wide"
+            initialChain={parseInt(process.env.NEXT_PUBLIC_DEFAULT_CHAIN!)}
+            chains={chains}
           >
-            <RainbowKitProvider
-              appInfo={{
-                appName: "Yamato Finance",
-                disclaimer: Disclaimer,
-              }}
-              theme={darkTheme({
-                accentColor: "#D66E36",
-                accentColorForeground: "white",
-                borderRadius: "medium",
-                fontStack: "system",
-              })}
-              modalSize="wide"
-              initialChain={parseInt(process.env.NEXT_PUBLIC_DEFAULT_CHAIN)}
-              chains={chains}
-            >
-              <Component {...pageProps} />
-            </RainbowKitProvider>
-          </RainbowKitSiweNextAuthProvider>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
         </SessionProvider>
       </WagmiConfig>
     </ChakraProvider>
