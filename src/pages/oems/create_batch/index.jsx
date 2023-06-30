a25d6b70d3f59ee9105f459890c0ff49de63a204
Sahil Harriram <sahil.harriram@gmail.com>
2023-06-30 14:41:14 +0700

    updated OEM window with correct subpages with basic layout

---
+++ src/pages/oems/create_batch/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+//import styles from "../styles/Home.module.css";
+import OemLayout from "../../../Components/OemLayout/OemLayout";
+import Head from "next/head";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+  NumberInput,
+  NumberInputField,
+  Spacer,
+  Spinner,
+} from "@chakra-ui/react";
+import DarkBackground from "Components/DarkBackground/DarkBackground";
+export default function create_batch() {
+  return (
+    <>
+      <OemLayout>
+        <Head>
+          <title>BlocTrace OEMS- create batch</title>
+          {/* <meta name="description" content="noindex,nofollow" /> */}
+        </Head>
+
+        <Heading
+          className="heading"
+          fontSize="5rem"
+          color="brand.0"
+          fontWeight="medium"
+          textAlign="center"
+        >
+          CREATE BATCH
+        </Heading>
+        <Flex
+          flexDirection={["column", "row"]}
+          justify="center"
+          gap="60px"
+          marginBottom="30px"
+        >
+          {/* User balances */}
+          <DarkBackground children={undefined}></DarkBackground>
+          {/* Mint Tokens */}
+          <DarkBackground children={undefined}></DarkBackground>
+        </Flex>
+      </OemLayout>
+    </>
+  );
+}
